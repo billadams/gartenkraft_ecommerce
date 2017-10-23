8b0ff887c85096dcedebd063ba5845c3e1f8615d
soerjawitaka <js190325@southeast.edu>
2017-10-23 16:38:38 -0500

    fix the links in layout to link to the correct controllers

--- Gartenkraft_Admin/Models/AccountViewModels.cs
+++ Gartenkraft_Admin/Models/AccountViewModels.cs
@@ -64,6 +64,13 @@ namespace Gartenkraft_Admin.Models
 
     public class RegisterViewModel
     {
+        [Display(Name = "Role")]
+        public string RoleName { get; set; }
+
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
@@ -79,6 +86,37 @@ namespace Gartenkraft_Admin.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Address1")]
+        public string Address1 { get; set; }
+
+        [Display(Name = "Address2")]
+        public string Address2 { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string Zip { get; set; }
+
+        [Display(Name = "")]
+        public string Zip4 { get; set; }
+
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageFileName { get; set; }
+
     }
 
     public class ResetPasswordViewModel
