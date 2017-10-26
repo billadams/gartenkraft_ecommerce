f788a9e2e6d68c419a30ce42e2b2ce7a38ad3e5b
soerjawitaka <js190325@southeast.edu>
2017-10-25 23:11:23 -0500

    vwInvoice, vwI_lineitem, vwProduct metadata

---
+++ Gartenkraft/Models/vwProduct_metadata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gartenkraft.Models
+{
+    [MetadataType(typeof(vwProduct_metadata))]
+    public partial class vwProduct { }
+
+    public class vwProduct_metadata
+    {
+        [Display(Name = "Product Name")]
+        public string product_name { get; set; }
+
+        [Display(Name = "Short Description")]
+        public string product_short_description { get; set; }
+
+        [Display(Name = "Long Description")]
+        public string product_long_description { get; set; }
+
+        [Display(Name = "Unit Price")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal product_unit_price { get; set; }
+
+        [Display(Name = "Weight")]
+        [RegularExpression(@"^\d+\.\d{0,2}$")]
+        public decimal product_weight { get; set; }
+    }
+}
