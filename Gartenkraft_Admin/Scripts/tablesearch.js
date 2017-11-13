f2e02a62d18cd149322552af167fc311d4a12c7b
Timothy M Hitchcock <timh412@inebraska.com>
2017-11-13 05:31:51 -0600

    Added deletecategory function to tablesearch.js

--- Gartenkraft_Admin/Scripts/tablesearch.js
+++ Gartenkraft_Admin/Scripts/tablesearch.js
@@ -103,5 +103,38 @@
                 tableBody.append('<tr class="search-sf"><td class="text-muted" colspan="6">No entries found.</td></tr>');
             }
         });
+        //delete category alert --------------------search Added by Tim H
+        $(function () {
+            $("#deletecategory").click(function (event) {
+                event.preventDefault();
+                $('<div title="Confirm Box"></div>').dialog({
+                    open: function (event, ui) {
+                        $(this).html("Yes or No question?");
+                    },
+                    close: function () {
+                        $(this).remove();
+                    },
+                    resizable: false,
+                    height: 140,
+                    modal: true,
+                    buttons: {
+                        'Yes': function () {
+                            var url = '@Url.Action(""Delete", new { id = item.product_id, @class = "btn btn-link" })';
+                            $(this).dialog('close');
+                            $.post(url, { categoryId: SelCat });
+
+                        },
+                        'No': function () {
+                            $(this).dialog('close');
+                            // $.post('url/theOtherValueYouWantToPAss');
+                        }
+                    }
+                });
+            });
+            //all tr elements are hidden
+            if (tableRowsClass.children(':visible').length == 0) {
+                tableBody.append('<tr class="search-sf"><td class="text-muted" colspan="6">No entries found.</td></tr>');
+            }
+        });
     })
 });
