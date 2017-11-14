3e3bc94f326f6fc3a8cd678defe0a97cc6a78b0c
quentin987 <mailqt987@yahoo.com>
2017-11-13 19:47:06 -0600

    viewmodel

---
+++ Gartenkraft/ViewModels/Checkout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gartenkraft.Models;
+
+namespace Gartenkraft.ViewModels
+{
+    public class Checkout : tblSales_Invoice
+    {
+    }
+}
