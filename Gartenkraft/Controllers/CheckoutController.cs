619ccdc9ed3dc4512a3d1eb5a20219523d8e7458
quentin987 <mailqt987@yahoo.com>
2017-11-15 17:26:26 -0600

    Checkout mockup

---
+++ Gartenkraft/Controllers/CheckoutController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Gartenkraft.Models;
+
+namespace Gartenkraft.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private Checkout oCheckout = new Checkout();
+        private GartenkraftEntities dbContext = new GartenkraftEntities();
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            if (Session["Cart"] != null)
+            {
+                oCheckout.CartInfo = (Cart) Session["Cart"];
+
+                return View(oCheckout);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Cart");//no cart found, shouldn't be able to view this page
+            }
+            
+        }
+    }
+}
