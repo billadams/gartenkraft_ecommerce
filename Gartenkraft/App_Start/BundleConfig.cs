52083e782cc286ded4ca3555c28a21b782d08c45
Bill Adams <billadams1977@gmail.com>
2017-12-10 09:57:02 -0600

    Minor changes, fixed half commented css file that I had previously fixed and somehow was merged back into master through another commit.

--- Gartenkraft/App_Start/BundleConfig.cs
+++ Gartenkraft/App_Start/BundleConfig.cs
@@ -6,27 +6,36 @@ namespace Gartenkraft {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/jquery.bcSwipe.js",
-                      "~/Scripts/toTop.js",
-                      "~/Scripts/edit.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js",
+                "~/Scripts/toTop.js",
+                "~/Scripts/edit.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap/bootstrap.min.css",
-                      "~/Content/bootstrap/theme.min.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap/bootstrap.min.css",
+                "~/Content/bootstrap/theme.min.css",
+                "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Areas/Admin/Content/css").Include(
+                "~/Areas/Admin/Content/bootstrap-theme.css",
+                "~/Areas/Admin/Content/bootstrap-theme.min.css",
+                "~/Areas/Admin/Content/bootstrap.css",
+                "~/Areas/Admin/Content/bootstrap.min.css",
+                "~/Areas/Admin/Content/dashboard.css",
+                "~/Areas/Admin/Content/sidebar.css",
+                "~/Areas/Admin/Content/Site.css",
+                "~/Areas/Admin/Content/table.css"));
         }
     }
 }
---
+++ Gartenkraft/Models/Shipping.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gartenkraft.Models
+{
+    public class Shipping
+    {
+
+        public int ShippingID { get; set; }
+        
+        public int CustomerID { get; set; }
+        [Required(ErrorMessage = "Please enter your First Name here.")]
+        [DisplayName("First Name")]
+        public string ShippingFirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your Last Name here.")]
+        [DisplayName("Last Name")]
+        public string ShippingLastName { get; set; }
+        [Required(ErrorMessage = "Please enter your Address here.")]
+        [DisplayName("Address")]
+        public string ShippingAddress { get; set; }
+
+        [DisplayName("Addresss 2")]
+        public string ShippingAddress2 { get; set; } = "";
+        [Required(ErrorMessage = "Please enter your City here.")]
+        [DisplayName("City")]
+        public string ShippingCity { get; set; }
+        [Required(ErrorMessage = "Please enter your state here.")]
+        [DisplayName("State")]
+        public string ShippingState { get; set; }
+        [Required(ErrorMessage = "Please enter your Zip Code here.")]
+        [DisplayName("Zip Code")]
+        public string ShippingZip { get; set; }
+
+        [DisplayName("Zip 4")]
+        public string ShippingZip4 { get; set; } = "";
+        [Required(ErrorMessage = "Please enter your Country here.")]
+        [DisplayName("Country")]
+        public string ShippingCountry { get; set; }
+    }
+}
